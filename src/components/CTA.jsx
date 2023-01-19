b6982705691b40da7ae594301826f1bbb4c76645
tobezhanabi <eliyahscopy@gmail.com>
2023-01-19 18:15:16 +0100

    almost

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
-const CTA = () => {
-  return <div>CTA</div>;
-};
+import styles from "../styles";
+import Button from "./Button";
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding}`}
+  >
+    <div>
+      <h2>Let's try our service now!</h2>
+      <p>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet
+      </p>
+    </div>
+    <div>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../styles";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Arcu totro, pruere jme oedii sed yo touyr tf is apir uhhde9ufg srd
+        oihdnohf asdj asjhurkjud aduhucfvjbc nf skljd sj soid idisd poanf
+        hduochs he ehopiusdjo rold sjhuh as sd d uhfs
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={`${layout.sectionImg}`}>
+      <img src={card} alt="" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
 export default CardDeal;
